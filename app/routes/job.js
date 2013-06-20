2bd7b2fec5de174fa3cdca95a2ab16d288e9826a
treehau5 <darethas@gmail.com>
2013-06-19 20:31:57 -0400

    added route jobs/new

--- app/routes/job.js
+++ app/routes/job.js
@@ -5,9 +5,11 @@
 var mongoose = require ("mongoose");
 
 var jobSchema = new mongoose.Schema({
-  position: { type: String, trim: true },
-  employer: { type: String, trim: true },
-  email: { type: String, trim: true }
+	title: { type: String, trim: true },
+		description: { type: String, trim: false },
+		employer: { type: String, trim: true },
+		email: { type: String, trim: true },
+		posting_date: { type: Date }
 });
 
 var Job = mongoose.model('Jobs', jobSchema);
@@ -18,28 +20,28 @@ var Job = mongoose.model('Jobs', jobSchema);
 
 // Remove all jobs from db 
 Job.remove({}, function(err) {
-  if (err) {
-    console.log ('Problem removing data');
-  }
+	if (err) {
+		console.log ('Problem removing data');
+	}
 });
 
 // Create a couple jobs
 var job1 = new Job ({
-  position: 'Front end web developer',
-  employer: 'Awesome startup',
-  email: 'teamlead@freebeer.com'
+	title: 'Front end web developer',
+		employer: 'Awesome startup',
+		email: 'teamlead@freebeer.com'
 });
 
 var job2 = new Job ({
-  position: 'JS guru',
-  employer: 'Boutique dev shop',
-  email: 'teamlead@hipsters.com'
+	title: 'JS guru',
+		employer: 'Boutique dev shop',
+		email: 'teamlead@hipsters.com'
 });
 
 var job3 = new Job ({
-  position: 'Web Designer Extraordinaire',
-  employer: 'Cutting edge design firm',
-  email: 'teamlead@madmen.com'
+	title: 'Web Designer Extraordinaire',
+		employer: 'Cutting edge design firm',
+		email: 'teamlead@madmen.com'
 });
 
 job1.save(function (err) {if (err) console.log ('Error saving job1');});
@@ -47,7 +49,11 @@ job2.save(function (err) {if (err) console.log ('Error saving job2');});
 job3.save(function (err) {if (err) console.log ('Error saving job3');});
 
 exports.list = function(req, res){
-  Job.find(function(err, result) {
-      res.render('jobs',{locals: {title:'result',jobs:result}});
-  });
+	Job.find(function(err, result) {
+		res.render('jobs',{locals: {title:'result',jobs:result}});
+	});
+};
+
+exports.new_job = function(req, res) {
+	res.render('new_job_form');
 };
