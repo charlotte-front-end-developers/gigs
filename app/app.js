3ea14842edb790b72ee94d5f425a15bccac64729
Matthew Stover <Matthew.C.Stover@lowes.com>
2013-08-21 20:31:17 -0400

    removed use app.router from our configure, inclusion was breaking users area

--- app/app.js
+++ app/app.js
@@ -27,7 +27,7 @@ function createApp() {
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
-    app.use(app.router);
+    //app.use(app.router);
     app.use(require('stylus').middleware(__dirname + '/public'));
     app.use(express.static(path.join(__dirname, 'public')));
     app.use(express.cookieParser());
